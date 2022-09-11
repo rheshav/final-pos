0a1062d9d5d3acaa3a5759bf2b665ef85140d3dc
rheshav <rhesa.havilah@gmail.com>
2022-09-12 00:20:11 +0700

    redux

--- src/Pos/Catalogue.js
+++ src/Pos/Catalogue.js
@@ -1,46 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+
+import { Container, Row, Col, Button, ListGroup } from "react-bootstrap";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import { useNavigate, Link } from "react-router-dom";
+
+import { getCatalogue, deleteCatalogue } from "../stores/catalogue";
 
 function Catalogue(props) {
-  console.log('props', props);
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
+  let navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const _data = useSelector((state) => state?.catalogue?.Data) || [];
+
+  const _trash = useSelector((state) => state?.catalogue?.Trash) || [];
+
+  const _loading = useSelector((state) => state?.catalogue?.Loading) || false;
+
+  const _input = useSelector((state) => state?.core?.Input) || {};
+
+  const _action = useSelector((state) => state?.catalogue?.Action) || "";
 
   useEffect(() => {
-    console.log('data', data);
-  }, [data]);
+    console.log("LOAD PERTAMA");
+    getCatalogue(props.type);
+  }, [props.type]);
 
   useEffect(() => {
-    setLoading(true);
-    fetch(props.type === 'food' ? 'https://jsonplaceholder.typicode.com/todos' : 'https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setLoading(false);
-          setData(result);
-        },
-        (error) => {
-          setLoading(false);
-          setData([]);
-          alert('Error');
-        }
-      );
-  }, [props?.type]);
+    if (_action === "DELETED_CATALOGUE") {
+      getCatalogue(props.type);
+    }
+  }, [_action]);
+
+  function openAdd() {
+    navigate("/addCatalogue/" + props.type);
+  }
+
+  function doDelete(_id) {
+    deleteCatalogue(_id);
+  }
 
   return (
     <div>
-      <p>{loading ? 'loading...' : 'Catalogue ' + props?.type + ' ' + data.length + ' items'} </p>
-      <Container>
-        <Row>
-          {data.map((value, key) => {
-            return (
-              <Col xs={4}>
-                {key + 1}. {value?.title}
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+      <Row>
+        <Col xs={9}>
+          <p>
+            {_loading
+              ? "Loading..."
+              : "Catalogue " +
+                props?.type +
+                " " +
+                _data.length +
+                " items (" +
+                _trash.length +
+                " items)"}
+          </p>
+        </Col>
+        <Col xs={3}>
+          <Button onClick={openAdd} variant="primary">
+            Create
+          </Button>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        {_data.map((value, key) => {
+          return (
+            <Col xs={4} style={{ marginBottom: 20 }}>
+              <ListGroup>
+                <ListGroup.Item>ID : {value?._id}</ListGroup.Item>
+                <ListGroup.Item>SKU : {value?.sku}</ListGroup.Item>
+                <ListGroup.Item>Nama : {value?.nama}</ListGroup.Item>
+                <ListGroup.Item>Kategori : {value?.kategori}</ListGroup.Item>
+                <ListGroup.Item>
+                  Harga :{" "}
+                  {new Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(value?.harga)}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Link
+                    style={{ fontWeight: "bold" }}
+                    to={"/editCatalogue/" + value?._id}
+                  >
+                    Edit
+                  </Link>
+                  <a
+                    style={{ marginLeft: 20, fontWeight: "bold" }}
+                    onClick={() => doDelete(value?._id)}
+                  >
+                    Delete
+                  </a>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 }
